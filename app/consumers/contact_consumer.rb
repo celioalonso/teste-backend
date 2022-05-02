aef34b6a7b651895583ff4411b283b5857df292b
Celio Teixeira Alonso <celioalonso@gmail.com>
2022-05-02 17:11:06 -0300

    Create Kafka LogConsumer

---
+++ app/consumers/contact_consumer.rb
@@ -0,0 +1,8 @@
+class ContactConsumer < Racecar::Consumer
+    subscribes_to "contacts_message"
+  
+    def process(message)
+      data = JSON.parse(message.value)
+      puts data
+    end
+  end
---
+++ app/consumers/log_consumer.rb
@@ -0,0 +1,8 @@
+class LogConsumer < Racecar::Consumer
+    subscribes_to "logs"
+  
+    def process(log)
+      data = JSON.parse(log.value)
+      puts data
+    end
+  end
