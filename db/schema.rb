6c7fb81e088ccdb33e1e5cfb9667fde74a35d4fb
Celio Alonso <celioalonso@gmail.com>
2022-05-04 12:29:31 -0300

    add docker , compose

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_02_003726) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "cpf_cnpj"
     t.string "cpfcnpj"
   end
 
